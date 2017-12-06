698485fcaa03faa990d951642d7f9f2302481363
zouhangte <zouhantezbm@126.com>
2017-12-06 17:29:02 +0800

    实现三层抽空四角锥网架

--- Assets/Core/Controller/FourAnglePyramidSpaceTrussGridFrame.cs
+++ Assets/Core/Controller/FourAnglePyramidSpaceTrussGridFrame.cs
@@ -23,7 +23,7 @@ public class FourAnglePyramidSpaceTrussGridFrame : WireFrameGenerater
 
     protected override WFData GenerateWFData(Clamp clamp)
     {
-        var startPos = -new Vector3(clamp.x_Size, clamp.height, clamp.y_Size) * 0.5f;
+        var startPos = -new Vector3(clamp.x_Size,- clamp.height, clamp.y_Size) * 0.5f;
         WFData wfData = new WFData();
         float x_Size = clamp.x_Size / clamp.x_num;
         float y_Size = clamp.y_Size / clamp.y_num;
--- Assets/Core/Utility/DefultTest.cs
+++ Assets/Core/Utility/DefultTest.cs
@@ -28,6 +28,7 @@ public class DefultTest : MonoBehaviour {
         抽空四角锥型网架,
         棋盘四角锥网架,
         三层正放四角锥网架,
+        三层抽空四角锥网架
     }
 
     public BarBehaiver bar;
@@ -74,6 +75,9 @@ public class DefultTest : MonoBehaviour {
                 break;
             case ControllerType.三层正放四角锥网架:
                 creater = new ThreeLayerPositiveFourPyramidGridFrame();
+                break;
+            case ControllerType.三层抽空四角锥网架:
+                creater = new ThreeLayerFourAnglePyramidGridFrame();
                 break;
             default:
                 break;
